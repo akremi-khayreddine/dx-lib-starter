ab8346bf3b1774cea6d1a58ec2126e15e28380d8
Akremi Khayreddine <48913326+akremi-khayreddine@users.noreply.github.com>
2020-02-12 12:51:33 +0100

    Create webhook.js

---
+++ .github/scripts/webhook.js
@@ -0,0 +1,46 @@
+const https = require('https')
+
+let RUN_ID = process.env.VERSION;
+let TRIGGER = process.env.TRIGGER;
+let TRIGGER_ID = process.env.TRIGGER_ID;
+let OUTPUT = process.env.OUTPUT ? process.env.OUTPUT : null;
+let OUTPUT_ID = process.env.OUTPUT_ID ? process.env.OUTPUT_ID : null;
+let WORKFLOW_ID = process.env.WORKFLOW_ID;
+let WEBHOOK_URL = "https://us-central1-locatus-test.cloudfunctions.net/checkSuite";
+let JOB = process.env.JOB;
+
+const data = JSON.stringify({
+  data: {
+    run_id: RUN_ID,
+    trigger: TRIGGER,
+    trigger_id: TRIGGER_ID,
+    output: OUTPUT
+    output_id: OUTPUT_ID,
+    repository: WORKFLOW_ID,
+    job: JOB
+  }
+})
+
+const options = {
+  hostname: 'https://us-central1-locatus-test.cloudfunctions.net',
+  path: '/checkSuite',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', (error) => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
