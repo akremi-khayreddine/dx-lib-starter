5d4f99dad14cf404349ceb439cc0486628f9d895
Akremi Khayreddine <48913326+akremi-khayreddine@users.noreply.github.com>
2020-02-12 11:43:41 +0100

    Update webhook.sh

--- .github/scripts/webhook.sh
+++ .github/scripts/webhook.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 set -eu
-
+   
 RUN_ID="$RUN_ID"
 TRIGGER="$TRIGGER"
 TRIGGER_ID="$TRIGGER_ID"
-OUTPUT="$OUTPUT"
-OUTPUT_ID="$OUTPUT_ID"
+OUTPUT=""
+OUTPUT_ID=""
 WORKFLOW_ID="$WORKFLOW_ID" 
 WEBHOOK_URL="https://us-central1-locatus-test.cloudfunctions.net/checkSuite"
 JOB=$JOB
 
-if [ -z "$TOKEN" ] || [ -z "$VERSION" ]; then
-  OUTPUT = ""
-  OUTPUT_ID = ""
+if [ -z "$OUTPUT" ] || [ -z "$OUTPUT_ID" ]; then
+else 
+    OUTPUT="$OUTPUT"
+    OUTPUT_ID="$OUTPUT_ID"
 fi
 
 curl -X POST -H "Content-Type: application/json" --data "{ \"data\": \"{ 'run_id': '$RUN_ID', 'trigger': '$TRIGGER', 'trigger_id': '$TRIGGER_ID', 'output': '$OUTPUT', 'output_id': '$OUTPUT_ID' ,'repository': '$WORKFLOW_ID', 'job': $JOB }\" }" $WEBHOOK_URL
