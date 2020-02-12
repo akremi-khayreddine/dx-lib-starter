32e08658e7d8206a4e5bff3bf45f018ed356ee8e
Akremi Khayreddine <48913326+akremi-khayreddine@users.noreply.github.com>
2020-02-12 13:25:41 +0100

    Update webhook.js

--- .github/scripts/webhook.js
+++ .github/scripts/webhook.js
@@ -9,7 +9,9 @@ let WORKFLOW_ID = process.env.WORKFLOW_ID;
 let WEBHOOK_URL = "https://us-central1-locatus-test.cloudfunctions.net/checkSuite";
 let JOB = process.env.JOB;
 
-const data =  JSON.stringify({ data: { 'run_id': '"+ RUN_ID +"', 'trigger': '"+TRIGGER+"', 'trigger_id': '"+TRIGGER_ID+"', 'output': '"+OUTPUT+"', 'output_id': '"+OUTPUT_ID+"', 'repository': '"+WORKFLOW_ID+"', 'job': "+JOB+" } });
+const payload = "{'run_id': '"+RUN_ID+"', 'trigger': '"+TRIGGER+"', 'trigger_id': '"+TRIGGER_ID+"', 'output': '"+OUTPUT+"', 'output_id': '"+OUTPUT_ID+"', 'repository': '"+WORKFLOW_ID+"', 'job': "+JOB+"}";
+
+const data =  "\"data\": " + payload;
 
 console.log(data);
 
