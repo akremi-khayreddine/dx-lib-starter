975ecbaa33553c31834307ae7b7a0c7bc5d87190
Akremi Khayreddine <48913326+akremi-khayreddine@users.noreply.github.com>
2020-02-12 01:15:16 +0100

    Update webhook.sh

--- .github/scripts/webhook.sh
+++ .github/scripts/webhook.sh
@@ -5,8 +5,8 @@ RUN_ID="$RUN_ID"
 TRIGGER="$TRIGGER"
 TRIGGER_ID="$TRIGGER_ID"
 WORKFLOW_ID="$WORKFLOW_ID" 
-WEBHOOK_URL = "$WEBHOOK_URL"
-JOB = $JOB
+WEBHOOK_URL="$WEBHOOK_URL"
+JOB=$JOB
 
 curl -X POST -H "Content-Type: application/json" 
      --data "{ \"data\": \"{ 'run_id': '$RUN_ID', 'trigger': '$TRIGGER', 'trigger_id': '$TRIGGER_ID', 'repository': '$WORKFLOW_ID', 'job': $JOB }\" }" $WEBHOOK_URL
