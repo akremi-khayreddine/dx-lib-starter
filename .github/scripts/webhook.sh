9cd8d020ddd15d044210405f700ca603379eae2a
Akremi Khayreddine <48913326+akremi-khayreddine@users.noreply.github.com>
2020-02-12 01:19:13 +0100

    Update webhook.sh

--- .github/scripts/webhook.sh
+++ .github/scripts/webhook.sh
@@ -8,5 +8,4 @@ WORKFLOW_ID="$WORKFLOW_ID"
 WEBHOOK_URL="https://us-central1-locatus-test.cloudfunctions.net/checkSuite"
 JOB=$JOB
 
-curl -X POST -H "Content-Type: application/json" 
-     --data "{ \"data\": \"{ 'run_id': '$RUN_ID', 'trigger': '$TRIGGER', 'trigger_id': '$TRIGGER_ID', 'repository': '$WORKFLOW_ID', 'job': $JOB }\" }" $WEBHOOK_URL
+curl -X POST -H "Content-Type: application/json" --data "{ \"data\": \"{ 'run_id': '$RUN_ID', 'trigger': '$TRIGGER', 'trigger_id': '$TRIGGER_ID', 'repository': '$WORKFLOW_ID', 'job': $JOB }\" }" $WEBHOOK_URL
